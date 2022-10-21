55b66d1032474e0efa4e134f5ccb44a1c70e9e4f
Roger Coll <rogercoll@protonmail.com>
2022-10-21 15:28:25 +0200

    add useradd and dwm

--- gentoo/configure.sh
+++ gentoo/configure.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# Install deps
 DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
 emerge --autounmask-continue -q $DEPLIST
+
+
+# Create user
+useradd -m -G users,wheel,audio -s /bin/bash neck
+passwd
+cp ../.bashrc /home/neck
+chmod neck:neck /home/neck/.bashrc
+echo "neck  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/neck
+
+# Setup dwm and slstatus
+su neck
+cd /home/neck/.config
+git clone https://github.com/rogercoll/dwm.git
+sudo make clean install
