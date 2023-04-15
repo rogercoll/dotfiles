63653fdb400ddf4f66e084ab99415950a93b48e6
Roger Coll <rogercoll@protonmail.com>
2023-04-15 17:10:04 +0200

    feat: add nvim and configure symlinks

--- configure.sh
+++ configure.sh
@@ -10,35 +10,21 @@ configure_dotfiles() {
 	ln -sf $current_dir/.gitconfig $HOME/.gitconfig
 	ln -sf $current_dir/.global_gitignore $HOME/.global_gitignore
 
-	echo "Configuring neovim, , this will override your neovim profile"
-	cp -a $current_dir/.config/nvim $HOME/.config
-	ln -sf $current_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-	nvim -c "PlugInstall"
+	echo "Setting tmux configuration file"
+	ln -sf $current_dir/.tmux.conf $HOME/.tmux.conf
+
+	echo "Setting alacritty configuration file"
+    mkdir -p $HOME/.config/alacritty
+	ln -sf $current_dir/.config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 }
 
-
-echo "Installing programming languages..."
-source ./install-langs.sh
-install_go
-install_rust
-
-echo "Installing tools and dependencies..."
-source ./install-deps.sh
-install_basics
-install_ripgrep
-install_fd
-install_bat
-install_neovim
-install_termshark
-install_difftastic
-
-
-echo "Installing suckless tools..."
-source ./install-suckless.sh
-install_slock
-install_dmenu
-
+configure_neovim() {
+    git clone git@github.com:rogercoll/nvim.git $HOME/.config/nvim || cd ~/.config/nvim/ && git pull
+}
 
 
 echo "Configuring dotfiles..."
 configure_dotfiles
+
+echo "Configuring neovim..."
+configure_neovim
