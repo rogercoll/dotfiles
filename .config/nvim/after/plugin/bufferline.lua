08d9b54d46f72ce07950ce71ef7327c78c20a73b
Roger Coll <rogercoll@protonmail.com>
2023-04-15 17:15:21 +0200

    V2: Just dotfiles! (#2)
    * refactor: remove all custom installation
    
    * feat: add NvChad submodule
    
    * feat: add nvim and configure symlinks

--- .config/nvim/after/plugin/bufferline.lua
+++
@@ -1,8 +0,0 @@
-require'bufferline'.setup {
-    icons = false,
-    closable = false,
-    clickable = false,
-    auto_hide = true,
-}
--- BufferPick
-vim.keymap.set("n", "<leader>bp", vim.cmd.BufferPick);
