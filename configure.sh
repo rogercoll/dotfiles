e2a7cc7e085cb7258e5fc95537a875c6f5c88ee7
Roger Coll <rogercoll@protonmail.com>
2023-05-02 11:30:10 +0200

    feat: new tmux configuration

--- configure.sh
+++ configure.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
 configure_dotfiles() {
-	local current_dir=`pwd`
-
 	echo "Setting bash profile, this will override your bash profile"
-	ln -sf $current_dir/.bashrc $HOME/.bashrc
+	ln -sf $OLDPWD/.bashrc $HOME/.bashrc
 
 	echo "Setting git profile, this will override your git profile"
-	ln -sf $current_dir/.gitconfig $HOME/.gitconfig
-	ln -sf $current_dir/.global_gitignore $HOME/.global_gitignore
+	ln -sf $OLDPWD/.gitconfig $HOME/.gitconfig
+	ln -sf $OLDPWD/.global_gitignore $HOME/.global_gitignore
 
 	echo "Setting tmux configuration file"
-	ln -sf $current_dir/.tmux.conf $HOME/.tmux.conf
+	ln -sf $OLDPWD/.tmux.conf $HOME/.tmux.conf
 
 	echo "Setting alacritty configuration file"
     mkdir -p $HOME/.config/alacritty
-	ln -sf $current_dir/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+	ln -sf $OLDPWD/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 }
 
 configure_neovim() {
     git clone git@github.com:rogercoll/nvim.git $HOME/.config/nvim || cd ~/.config/nvim/ && git pull
 }
 
+configure_tmux() {
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
+
+echo "Configuring neovim..."
+configure_neovim
+
+echo "Configuring tmux..."
+configure_tmux
 
 echo "Configuring dotfiles..."
 configure_dotfiles
 
-echo "Configuring neovim..."
-configure_neovim
